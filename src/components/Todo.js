1f32f0a742a6bb71010037e3ba947993c71bd585
Jim Christian Edullantes <jimchristianedullantes9@gmail.com>
2023-04-02 00:43:52 +0800

    Added Submit handler

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,14 +3,25 @@ import { useState } from "react";
 export default function Todo() {
 
     const [todo, setTodo] = useState("");
+    const [todoList, setTodoList] = useState([]);
 
     const handleChange = (event) => {
         setTodo(event.target.value);
     }
 
+    const handleSubmit = (event) => {
+
+        event.preventDefault();
+        let tempList = todoList;
+        tempList.push(todo);
+        setTodoList(tempList);
+        setTodo("");
+        console.log(todoList);
+    }
+
     return (
         <div>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <input onChange={handleChange} type="text" value={todo}/>
                 <button type="submit">Add</button>
             </form>
