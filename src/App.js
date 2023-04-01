440bd9ed4a7c3b67899e1d26d192d576d5fb90ee
Jim Christian Edullantes <jimchristianedullantes9@gmail.com>
2023-04-02 01:56:38 +0800

    Added CSS stylesheet

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Todo from "./components/Todo";
 import ListItem from "./components/ListItem";
 import InlineComponent from "./components/InlineComponent";
+import StylesheetComponent from "./components/StylesheetComponent";
 
 function App() {
   return (
     <div className="App">
       <Todo/>
       <InlineComponent/>
+      <StylesheetComponent/>
     </div>
   );
 }
