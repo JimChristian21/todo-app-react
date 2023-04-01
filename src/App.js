9f4b20f74f018f581b4933bfeab8e4506e95d53f
Jim Christian Edullantes <jimchristianedullantes9@gmail.com>
2023-04-02 01:51:46 +0800

    Added inline style

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 import './App.css';
 import Todo from "./components/Todo";
 import ListItem from "./components/ListItem";
+import InlineComponent from "./components/InlineComponent";
 
 function App() {
   return (
     <div className="App">
       <Todo/>
+      <InlineComponent/>
     </div>
   );
 }
