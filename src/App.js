67725ef1b450aba24ac08093b4befb87d80db457
Jim Christian Edullantes <jimchristianedullantes9@gmail.com>
2023-04-02 16:56:23 +0800

    Added todo list component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import {useState} from "react";
 import Header from "./components/Header";
 import Form from "./components/Form";
+import Todo from "./components/Todo";
 
 function App() {
 
   const [todo, setTodo] = useState('');
   const [todoList, setTodoList] = useState([]);
-  console.log(todoList);
+  const todos = todoList.map((task, index) => {
+    return (
+      <Todo key={index}/>
+    );
+  });
 
   return (
     <div className="App">
       <Header/>
       <Form todo={todo} setTodo={setTodo} todoList={todoList} setTodoList={setTodoList}/>
+      {todos}
     </div>
   );
 }
