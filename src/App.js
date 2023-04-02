4effb645bba28e5594435eae44d53213236828c8
Jim Christian Edullantes <jimchristianedullantes9@gmail.com>
2023-04-02 15:02:55 +0800

    Deleted all components for refresh for the new todo app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 // import logo from './logo.svg';
 import './App.css';
-import Todo from "./components/Todo";
-import ListItem from "./components/ListItem";
-import InlineComponent from "./components/InlineComponent";
-import StylesheetComponent from "./components/StylesheetComponent";
-import ModuleComponent from "./components/ModuleComponent";
 
 function App() {
   return (
     <div className="App">
-      <Todo/>
-      <InlineComponent/>
-      <StylesheetComponent/>
-      <ModuleComponent/>
+      
     </div>
   );
 }
--- src/components/ModuleComponent.js
+++
@@ -1,9 +0,0 @@
-import styles from "./style.module.css";
-
-export default function ModuleComponent() {
-
-    console.log(styles);
-    return (
-        <div className={styles.header}>Green</div>
-    )
-}
